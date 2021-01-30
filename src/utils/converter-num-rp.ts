d2e53e19ef94af3a91df5f3973920ef99775d335
Nurdiansyah <nur.diansyah.ckt@gmail.com>
2021-01-30 16:55:01 +0700

    fix utils

--- src/utils/converter-num-rp.ts
+++ src/utils/converter-num-rp.ts
@@ -1,6 +1,6 @@
 export const convertToRp = (value: number, thousandSeparator: string = ".", decimalSeparator: string = ",") => {
-  return value
-    .toFixed(2)
+  return `${value
+    .toString()
     .replace(".", decimalSeparator)
-    .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
+    .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.")},-`;
 };
