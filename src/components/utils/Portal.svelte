27cc265135a3c4f1ff472ffc0a92aeac5c27896d
Nurdiansyah <nur.diansyah.ckt@gmail.com>
2021-07-03 16:16:38 +0700

    fix modal

---
+++ src/components/utils/Portal.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { onMount } from "svelte";
+  let ref;
+  let portal;
+  onMount(() => {
+    // Create and append a DOM node to `document.body`
+    // and render the component into it.
+    portal = document.createElement("div");
+    portal.setAttribute("data-svelte-dialog-portal", "true");
+    document.body.appendChild(portal);
+    portal.appendChild(ref);
+    return () => {
+      document.body.removeChild(portal);
+    };
+  });
+</script>
+
+<div bind:this={ref} class={$$props.class}>
+  <slot />
+</div>
