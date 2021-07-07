13cd53b9f52d0e95cc5dd4165875fa03b62a8f0b
Nurdiansyah <nur.diansyah.ckt@gmail.com>
2021-07-07 07:19:06 +0700

    clean bank

--- src/pages/transaksi/bank/[bankId]/statements/_layout.svelte
+++ src/pages/transaksi/bank/[bankId]/statements/_layout.svelte
@@ -6,6 +6,9 @@
   import PageLayout from "__@root/layout/PageLayout.svelte";
   import { stores } from "@deboxsoft/accounting-client";
   import { getApplicationContext } from "__@modules/app";
+  import Dropdown from "__@comps/Dropdown.svelte";
+  import DropdownToggle from "__@comps/DropdownToggle.svelte";
+  import { createReportContext } from "./_export";
 
   import TableStatementBank from "./_components/TableStatementBank.svelte";
   // import DatePickr from "__@comps/DatePickr.svelte";
@@ -14,6 +17,7 @@
   setBreadcrumbContext({ path: $url("./"), title: "Rekonsiliasi Detail" });
   const applicationContext = getApplicationContext();
   const { notify, loading: loadingApp } = applicationContext;
+  const reportContext = createReportContext();
 
   let loading = true;
   const { getBank } = stores.getBankContext();
@@ -69,6 +73,21 @@
       $loadingApp = false;
     }
   }
+
+  const createExportMenuHandler = (close) => ({
+    pdf: () => {
+      reportContext.pdf(bankStatementList);
+      close();
+    },
+    csv: () => {
+      reportContext.csv(bankStatementList);
+      close();
+    },
+    print: () => {
+      reportContext.print(bankStatementList);
+      close();
+    }
+  });
 </script>
 
 <PageLayout breadcrumb={[]}>
@@ -81,6 +100,41 @@
       <i class="icon-file-spreadsheet2 mr-1" />
       Rekonsiliasi
     </a>
+    <Dropdown class="breadcrumb-elements-item dropdown p-0">
+      <DropdownToggle class="breadcrumb-elements-item" caret nav>
+        <i class="icon-file-download2 mr-1" />
+        Ekspor
+      </DropdownToggle>
+      <svelte:fragment slot="menu" let:closeHandler={dropdownClose}>
+        <a
+          href="/#"
+          target="_self"
+          on:click|preventDefault={createExportMenuHandler(dropdownClose).pdf}
+          class="dropdown-item"
+        >
+          <i class="icon-file-pdf" />
+          Download PDF</a
+        >
+        <a
+          href="/#"
+          target="_self"
+          on:click|preventDefault={createExportMenuHandler(dropdownClose).csv}
+          class="dropdown-item"
+        >
+          <i class="icon-file-excel" />
+          Download CSV</a
+        >
+        <a
+          href="/#"
+          target="_self"
+          on:click|preventDefault={createExportMenuHandler(dropdownClose).print}
+          class="dropdown-item"
+        >
+          <i class="icon-printer2" />
+          Print</a
+        >
+      </svelte:fragment>
+    </Dropdown>
   </svelte:fragment>
   <div class="card flex-column flex-1 d-flex">
     <div class="card-body d-flex flex-column flex-1">
