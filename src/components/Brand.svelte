6634f616890959e6ac209367ab637852f29311cc
Nurdiansyah <nur.diansyah.ckt@gmail.com>
2021-07-28 02:27:55 +0700

    Brand.svelte

---
+++ src/components/Brand.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { stores } from "@deboxsoft/accounting-client";
+  import { url } from "@roxi/routify";
+  import { getApplicationContext } from "__@modules/app";
+  import initial from "initials";
+
+  const { config } = getApplicationContext();
+  const { companyStore } = stores.getCompanyContext();
+
+  let imageFail = false,
+    imageLoading = true;
+  $: srcImg = $config.brandUrl;
+</script>
+
+<div class="navbar-brand text-center text-lg-left wmin-200">
+  <a href={$url("/")} class="d-flex">
+    {#if srcImg && !imageFail}
+      <img
+        src={srcImg}
+        on:error={() => {
+          imageLoading = false;
+          imageFail = true;
+        }}
+        on:load={() => {
+          imageFail = false;
+          imageLoading = false;
+        }}
+      />
+    {/if}
+    <div>
+      {#if srcImg && imageFail}
+        {initial($companyStore.name)?.toUpperCase() || "LC"} |
+      {/if}
+      {$companyStore.unit || ""}
+    </div>
+  </a>
+</div>
