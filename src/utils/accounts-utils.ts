d25adaef91a2295324b43609cb96995d02a4a594
Nurdiansyah <nur.diansyah.ckt@gmail.com>
2021-08-21 11:23:35 +0700

    clean

--- src/utils/accounts-utils.ts
+++ src/utils/accounts-utils.ts
@@ -6,7 +6,6 @@ export function filteringAccountDebit(accountStore: Readable<Account[]>) {
   const { authenticationStore } = getAuthenticationContext();
   let accountsIdDebit = get(authenticationStore).metadata?.debitAccounts;
   return derived(accountStore, (_) => {
-    console.log(get(authenticationStore).metadata);
     return _.filter((_) => {
       if (/^[^4].*/g.test(_.id)) {
         if (!accountsIdDebit || accountsIdDebit.includes(_.id)) {
