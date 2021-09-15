56f4e2e7fd02b63c61dcf2ea16b74fcb5d7baa54
Nurdiansyah <nur.diansyah.ckt@gmail.com>
2021-09-15 07:37:06 +0700

    fix: components

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,14 +1,15 @@
 <script>
   export let submitting = false;
   export let disabled = false;
+  export let disableMarginRight = false;
   const { class: className } = $$props;
 
   $: _disabled = disabled || submitting;
 </script>
 
-<button {...$$restProps} class="button {className}" class:btn-loading={submitting} disabled={_disabled} on:click>
+<button {...$$restProps} class="btn {className}" class:btn-loading={submitting} disabled={_disabled} on:click>
   {#if submitting}
-    <i class="icon-spinner4 spinner mr-2" />
+    <i class="icon-spinner4 spinner" class:mr-2={!disableMarginRight} />
     proses
   {:else}
     <slot />
---
+++ src/components/forms/InputPasswordField.svelte
@@ -0,0 +1,88 @@
+<script>
+  import { writable } from "svelte/store";
+  import { getFormContext } from "__@stores/form";
+  import { createEventDispatcher } from "svelte";
+  import Button from "../Button.svelte";
+
+  const { validateField, fields = writable({}), fieldsErrors, submitted } = getFormContext() || {};
+  const dispatcher = createEventDispatcher();
+  export let name;
+  export let value;
+  export let id = name;
+  export let ref;
+  const { class: className } = $$props;
+
+  let invalid = true,
+    msgError,
+    isPlain = false;
+
+  $: {
+    if ($fieldsErrors && $fieldsErrors[name]) {
+      invalid = true;
+      msgError = Array.isArray($fieldsErrors[name])
+        ? $fieldsErrors[name].length > 0 && $fieldsErrors[name][0]
+        : $fieldsErrors[name];
+    } else {
+      invalid = false;
+    }
+  }
+
+  function createInputHandler() {
+    const _validate = validateField(name);
+    return (e) => {
+      _validate();
+      dispatcher("input", e);
+    };
+  }
+
+  function toggleHandler() {
+    isPlain = !isPlain;
+  }
+</script>
+
+<div class="input-group">
+  {#if isPlain}
+    <input
+      {...$$restProps}
+      {id}
+      type="text"
+      class={className}
+      bind:this={ref}
+      bind:value={$fields[name]}
+      class:is-valid={$submitted && !invalid}
+      class:is-invalid={$submitted && invalid}
+      on:input={createInputHandler()}
+      on:keydown
+      on:keypress
+    />
+    <div class="input-group-append">
+      <Button class="btn btn-light btn-icon" style="height: 36px"on:click={toggleHandler} tabindex="-1">
+        <i class="icon-eye-blocked" />
+      </Button>
+    </div>
+  {:else}
+    <input
+      {...$$restProps}
+      {id}
+      type="password"
+      class={className}
+      bind:this={ref}
+      bind:value={$fields[name]}
+      class:is-valid={$submitted && !invalid}
+      class:is-invalid={$submitted && invalid}
+      on:input={createInputHandler()}
+      on:keydown
+      on:keypress
+    />
+    <div class="input-group-append">
+      <Button class="btn btn-light btn-icon" style="height: 36px"on:click={toggleHandler} tabindex="-1">
+        <i class="icon-eye" />
+      </Button>
+    </div>
+  {/if}
+  {#if $submitted}
+    {#if invalid}
+      <p class="invalid-tooltip">{msgError}</p>
+    {/if}
+  {/if}
+</div>
