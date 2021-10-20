f51d71ffdd94726b08fe7977cc756fcceefb8090
Nurdiansyah <nur.diansyah.ckt@gmail.com>
2021-10-20 09:47:16 +0700

    fix dashboard

--- src/pages/dashboard/index.svelte
+++ src/pages/dashboard/index.svelte
@@ -1,57 +1,40 @@
 <!--routify:options title="dashboard"-->
 <script>
-  import { url } from "@roxi/routify";
   import PageLayout from "__@root/layout/PageLayout.svelte";
+  import MenuCard from "__@root/pages/bdd/_components/MenuCard.svelte";
+  import { getAuthenticationContext } from "__@modules/users";
+  import { get } from "svelte/store";
+
+  const auth = getAuthenticationContext();
+  const profile = get(auth.authenticationStore);
+  const accountShow = auth.getQuery("account").read().granted;
+  const programShow = auth.getQuery("program").read().granted;
+  const transactionShow = auth.getQuery("transaction").read().granted;
+  const transactionOwnShow = auth.getQuery("transaction").readOwn().granted;
+  const ledgerShow = auth.getQuery("ledger").read().granted;
+  const bankShow = auth.getQuery("bank").read().granted;
+  const cashierShow = auth.getQuery("cashier").read().granted;
+  const balanceSheetShow = auth.getQuery("balanceSheet").read().granted;
+  const statementIncomeShow = auth.getQuery("statementIncome").read().granted;
+  const inventoryShow = auth.getQuery("inventory").read().granted;
+  const bddShow = auth.getQuery("bdd").read().granted;
+  const stockShow = auth.getQuery("stock").read().granted;
+  const userShow = auth.getQuery("user").read().granted;
+  const settingShow = auth.getQuery("setting").read().granted;
+  const reportShow = ledgerShow || statementIncomeShow || balanceSheetShow;
 </script>
 
-<PageLayout headerPageDisable>
-  <div class="row">
-    <div class="col-xl-7">
-      <div class="card" style="height: 100%; width: 100%">
-        <div class="card-header">
-          <h5 class="card-title">Transaksi</h5>
-        </div>
-        <div class="card-body">
-          <div class="row">
-            <div class="col-sm-4">
-              <div class="d-flex align-items-center justify-content-center">
-                <a href={$url("/transaksi/jurnal")}>Transaksi</a>
-              </div>
-            </div>
-            <div class="col-sm-4">
-              <div class="d-flex align-items-center justify-content-center">
-                <a href={$url("/transaksi/buku-besar")}>Buku Besar</a>
-              </div>
-            </div>
-            <div class="col-sm-4">
-              <div class="d-flex align-items-center justify-content-center">
-                <a href={$url("/transaksi/bank")}>Rekonsiliasi Bank</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-xl-5">
-      <div class="card" style="height: 100%; width: 100%">
-        <div class="card-header">
-          <h5 class="card-title">Pengikhtisaran</h5>
-        </div>
-        <div class="card-body">
-          <div class="row">
-            <div class="col-sm-6">
-              <div class="d-flex align-items-center justify-content-center">
-                <a href={$url("/pengikhtisaran/neraca")}>Neraca</a>
-              </div>
-            </div>
-            <div class="col-sm-6">
-              <div class="d-flex align-items-center justify-content-center">
-                <a href={$url("/pengikhtisaran/laba-rugi")}>Laba-Rugi</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+<PageLayout>
+  <div class="d-flex flex-wrap">
+    <MenuCard show={transactionShow} label="Jurnal" path="/transaksi/jurnal" iconClass="icon-loop"  />
+    <MenuCard show={cashierShow} label="Kasir" path="/transaksi/cashier" iconClass="icon-cash2" />
+    <MenuCard show={bankShow} label="Bank" path="/transaksi/bank" iconClass="icon-cash4" />
+    <MenuCard show={stockShow} label="Persediaan" path="/persediaan/home" iconClass="icon-archive" />
+    <MenuCard show={inventoryShow} label="Aktiva Tetap" path="/aktiva-tetap/home" iconClass="icon-file-check" />
+    <MenuCard show={bddShow} label="BDD" path="/bdd/home" iconClass="icon-library2" />
+    <MenuCard show={reportShow} label="Laporan" path="/laporan" iconClass="icon-stack-text" />
+    <MenuCard show={userShow} label="Users" path="/users" iconClass="icon-users4" />
+    <MenuCard show={settingShow} label="Settings" path="/settings" iconClass="icon-cog3" />
+    <MenuCard show={profile.authenticated} label="Profile" path="/profile" iconClass="icon-profile"/>
   </div>
 </PageLayout>
