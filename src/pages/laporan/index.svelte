d499e206a8fdd713836271b363bc2c60f4703dcc
Nurdiansyah <nur.diansyah.ckt@gmail.com>
2021-10-20 09:44:40 +0700

    fix laporan

--- src/pages/laporan/index.svelte
+++ src/pages/laporan/index.svelte
@@ -1,14 +1,31 @@
+<!--routify:options title="Menu"-->
 <script>
   import { getAuthenticationContext } from "__@modules/users";
-  import { redirect } from "@roxi/routify";
-  const authContext = getAuthenticationContext();
-  const statementIncomeShow = auth.getQuery().read("statementIncome").granted;
-  const balanceSheetShow = auth.getQuery().read("balanceSheet").granted;
-  if (statementIncomeShow) {
-    $redirect("./laba-rugi");
-  } else if (balanceSheetShow) {
-    $redirect("./neraca");
-  } else {
-    $redirect("/access-denied")
-  }
+  import PageLayout from "__@root/layout/PageLayout.svelte";
+  import MenuCard from "__@root/pages/bdd/_components/MenuCard.svelte";
+  const auth = getAuthenticationContext();
+  const ledgerShow = auth.getQuery("ledger").read().granted;
+  const balanceSheetShow = auth.getQuery("balanceSheet").read().granted;
+  const statementIncomeShow = auth.getQuery("statementIncome").read().granted;
 </script>
+
+<PageLayout>
+  <div class="d-flex flex-wrap">
+    <MenuCard show={ledgerShow} label="Trial Balance" path="./trial-balance" iconClass="icon-table2" />
+    <MenuCard show={ledgerShow} label="Buku Besar" path="./buku-besar" iconClass="icon-file-spreadsheet" />
+    <MenuCard
+      show={statementIncomeShow}
+      label="Laba Rugi Perkiraan"
+      path="./laba-rugi-perkiraan"
+      iconClass="icon-stack"
+    />
+    <MenuCard show={statementIncomeShow} label="Laba Rugi" path="./laba-rugi" iconClass="icon-file-text2" />
+    <MenuCard
+      show={balanceSheetShow}
+      label="Neraca Perkiraan"
+      path="./neraca-perkiraan"
+      iconClass="icon-file-presentation"
+    />
+    <MenuCard show={balanceSheetShow} label="Neraca" path="./neraca" iconClass="icon-file-stats" />
+  </div>
+</PageLayout>
