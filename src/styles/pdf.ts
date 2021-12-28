3aebdcaac9baa5b6b9eff0f94eb2171b5786a773
Nurdiansyah <nur.diansyah.ckt@gmail.com>
2021-12-28 21:10:51 +0700

    add roboto pdf

--- src/styles/pdf.ts
+++ src/styles/pdf.ts
@@ -3,10 +3,10 @@ import vfsFonts from "./vfs_fonts";
 
 const fonts = {
   Roboto: {
-    normal: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Regular.ttf",
-    bold: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Medium.ttf",
-    italics: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Italic.ttf",
-    bolditalics: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-MediumItalic.ttf"
+    normal: "Roboto-Regular.ttf",
+    bold: "Roboto-Medium.ttf",
+    italics: "Roboto-Italic.ttf",
+    bolditalics: "Roboto-MediumItalic.ttf"
   }
 };
 
