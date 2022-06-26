ed364eb652bfd6e12223f7e3346936b0e269b543
Nurdiansyah <nur.diansyah.ckt@gmail.com>
2022-06-26 18:39:39 +0700

    feat: sort-utils.ts add type date compareable

--- src/utils/sort-utils.ts
+++ src/utils/sort-utils.ts
@@ -4,21 +4,27 @@
  * ex : [].sort(sortUtilsFunc)
  */
 
+import { dayjs } from "@deboxsoft/accounting-api";
+
 export const sortUtilsFunc = (key, order: "asc" | "desc" = "asc") => {
   return (a, b) => {
-    if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+    if (!Object.prototype.hasOwnProperty.call(a, key) || !Object.prototype.hasOwnProperty.call(b, key)) {
       // property doesn't exist on either object
       return 0;
     }
 
-    const varA = typeof a[key] === "string" ? a[key].toUpperCase() : a[key];
-    const varB = typeof b[key] === "string" ? b[key].toUpperCase() : b[key];
-
     let comparison = 0;
-    if (varA > varB) {
-      comparison = 1;
-    } else if (varA < varB) {
-      comparison = -1;
+    if (a[key] instanceof Date) {
+      comparison = dayjs(a[key]).isAfter(b[key]) ? 1 : -1;
+    } else {
+      const varA = typeof a[key] === "string" ? a[key].toUpperCase() : a[key];
+      const varB = typeof b[key] === "string" ? b[key].toUpperCase() : b[key];
+
+      if (varA > varB) {
+        comparison = 1;
+      } else if (varA < varB) {
+        comparison = -1;
+      }
     }
     return order === "desc" ? comparison * -1 : comparison;
   };
