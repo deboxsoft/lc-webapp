c890c14d14650ee9a4e9f28679c1cc6436b9324c
Nurdiansyah <nur.diansyah.ckt@gmail.com>
2022-07-15 14:16:02 +0700

    revisi

--- src/pages/laporan/saldo-perkiraan/[id]/index.svelte
+++ src/pages/laporan/saldo-perkiraan/[id]/index.svelte
@@ -102,8 +102,8 @@
           class="dropdown-item"
         >
           <i class="icon-file-pdf" />
-          Download PDF</a
-        >
+          Download PDF
+        </a>
         <a
           href="/#"
           target="_self"
@@ -111,8 +111,8 @@
           class="dropdown-item"
         >
           <i class="icon-file-excel" />
-          Download CSV</a
-        >
+          Download CSV
+        </a>
         <a
           href="/#"
           target="_self"
@@ -120,14 +120,14 @@
           class="dropdown-item"
         >
           <i class="icon-printer2" />
-          Print</a
-        >
+          Print
+        </a>
       </svelte:fragment>
     </Dropdown>
   </svelte:fragment>
   <div class="card d-flex flex-1">
     <div class="card-body d-flex flex-1 flex-column">
-      {#if $loading || !$account}
+      {#if !$account}
         <Loader />
       {:else}
         <div class="border-bottom-grey-600 border-bottom-1 mb-1 pb-1">
@@ -143,8 +143,9 @@
         <LedgerAccountTable generalLedgerList={$dataStore}>
           {#if $pageInfo.hasNext}
             <div class="" style="height: 50px">
-              <Button class="btn btn-light w-100 text-uppercase" on:click={infiniteHandler} {submitting}
-                ><i class="icon-chevron-down mr-2" />Muat Lebih Banyak...
+              <Button class="btn btn-light w-100 text-uppercase" on:click={infiniteHandler} {submitting}>
+                <i class="icon-chevron-down mr-2" />
+                Muat Lebih Banyak...
               </Button>
             </div>
           {/if}
